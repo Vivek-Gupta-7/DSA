a256b40ce36fcd0de10f8fefc5b7069757f217e7
Vivek-Gupta-7 <vivekgupta1st@gmail.com>
2026-06-28 22:40:03 +0530

    Update Triangle.java

--- Assignment/src/Conditional/Triangle.java
+++ Assignment/src/Conditional/Triangle.java
@@ -9,6 +9,7 @@ public class Triangle {
         int Height= in.nextInt();
         System.out.println("Enter the Base of Triangle");
         int Base= in.nextInt();
+        // THE FORMULA TO CALCULATE THE AREA OF TRIANGLE IS 1/2*HEIGHT*BASE
         double result=0.5*Height*Base;
         System.out.println("The area of the Triangle is" + " "+ result);
         in.close();
