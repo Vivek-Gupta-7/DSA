e294aff7d283479fcf822be71714271cbba66d44
Vivek-Gupta-7 <vivekgupta1st@gmail.com>
2026-06-28 22:38:36 +0530

    Update Fibonacci.java

--- Assignment/src/Conditional/Fibonacci.java
+++ Assignment/src/Conditional/Fibonacci.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        // SCANNER WILL HELP TO TAKE THE INPUT
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of terms for fibonacci series");
         int n = in.nextInt();
+        // WE HAVE TAKE THREE VARIABLES, IT WILL HELP TO SWAP VALUES . 
         int a=0;
         int b=1;
         int c=0;
